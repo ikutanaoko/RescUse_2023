d5b8a39bbcd9338aaf45ec067dac5d527484f0b1
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-07-04 10:21:22 +0000

    [add]logo_07041921

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  
+  # 社員用
+  devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about', as: 'about'
+    get '/index' => 'homes#index', as: 'index'
+  end
+  
+  # 管理者用
+  devise_for :admins,skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+  namespace :admin do
+    get '/' => 'home#top'
+    get '/result'  => 'home#result'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
